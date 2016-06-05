63f0b1d57490e5ca78ff3c2012ddd4d11fae8df6
Shun <s.n.systems.engineer@gmail.com>
2016-06-05 18:53:44 +0900

    Update 09.py

--- Chap-01/09.py
+++ Chap-01/09.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import random
+
+
+def Typoglycemia(text):
+    text_list = text.split(" ")
+    random_text = ""
+    random_text_list = []
+    for i in text_list:
+        if(len(i) <= 4):
+            random_text_list.append(i)
+        else:
+            random_word = i[:1:]
+            random_str = list(i[1:-1:])
+            random.shuffle(random_str)
+            random_word += ''.join(random_str)
+            random_word += i[-1::]
+            random_text_list.append(random_word)
+        random_text = " ".join(random_text_list)
+    return random_text
+
+
+random_text = Typoglycemia(("I couldn't believe that"
+                            " I could actually understand "
+                            "what I was reading : "
+                            "the phenomenal power of the human mind ."))
+print random_text
