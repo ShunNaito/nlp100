855d9d01d0eb330ba45277c4e09cbf6ad83a0554
Shun <s.n.systems.engineer@gmail.com>
2016-07-11 20:29:58 +0900

    Add 23.py

---
+++ Chap-03/23.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+import json
+import re
+
+r = re.compile('==+(\s)*(.+?)(\s)*==+')
+
+f = open('jawiki-country.json', 'r')
+lines = f.readlines()
+f.close()
+
+for line in lines:
+    article_dict = json.loads(line)
+    if article_dict["title"] == u"イギリス":
+        England_text = article_dict["text"]
+
+lines = England_text.split("\n")
+for line in lines:
+    m = r.search(line)
+    if m is not None:
+        print m.group(2).encode('utf_8')
