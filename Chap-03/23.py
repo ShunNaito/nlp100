48eb957cb2d6d6780a18679ee367e7438625c8a3
Shun <s.n.systems.engineer@gmail.com>
2016-07-12 18:52:35 +0900

    Update 23.py

--- Chap-03/23.py
+++ Chap-03/23.py
@@ -2,7 +2,8 @@
 import json
 import re
 
-r = re.compile('==+(\s)*(.+?)(\s)*==+')
+# r = re.compile('==+(\s)*(.+?)(\s)*==+')
+r = re.compile('==(=+)*(\s)*(.+?)(\s)*(=+)*==')
 
 f = open('jawiki-country.json', 'r')
 lines = f.readlines()
@@ -14,7 +15,16 @@ for line in lines:
         England_text = article_dict["text"]
 
 lines = England_text.split("\n")
+
 for line in lines:
     m = r.search(line)
     if m is not None:
-        print m.group(2).encode('utf_8')
+        level = 1
+        if m.group(1) is None:
+            print m.group(3).encode('utf_8')
+            print level
+        else:
+            for text in m.group(1):
+                level = level + 1
+            print m.group(3).encode('utf_8')
+            print level
