216f787a2eac1c722eab365090c382b6a206229b
Shun <s.n.systems.engineer@gmail.com>
2016-07-14 19:55:29 +0900

    Update 24.py

--- Chap-03/24.py
+++ Chap-03/24.py
@@ -2,7 +2,7 @@
 import json
 import re
 
-r = re.compile(u'File:(.*\.jpg)|ファイル:(.*\.jpg)')
+r = re.compile(u'(File|ファイル):(.+?)\|')
 
 f = open('jawiki-country.json', 'r')
 lines = f.readlines()
@@ -18,7 +18,4 @@ lines = England_text.split("\n")
 for line in lines:
     m = r.search(line)
     if m is not None:
-        if m.group(1) is not None:
-            print m.group(1).encode('utf_8')
-        if m.group(2) is not None:
-            print m.group(2).encode('utf_8')
+        print m.group(2).encode('utf_8')
