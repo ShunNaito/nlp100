3910ff46d34bd5e962b0ef17097cf73e2500aaca
Shun <s.n.systems.engineer@gmail.com>
2016-07-16 17:35:14 +0900

    Update 25.py

--- Chap-03/25.py
+++ Chap-03/25.py
@@ -2,7 +2,7 @@
 import json
 import re
 
-r = re.compile(u'\|(.*)\s=\s(.*)')
+r = re.compile(u'\|(.*)\s=\s(.*)|(\*+\{\{lang.*)')
 
 f = open('jawiki-country.json', 'r')
 lines = f.readlines()
@@ -18,4 +18,7 @@ lines = England_text.split("\n")
 for line in lines:
     m = r.search(line)
     if m is not None:
-        print m.group(1).encode('utf_8'), m.group(2).encode('utf_8')
+        if m.group(3) is not None:
+            print m.group(3).encode('utf_8')
+        else:
+            print m.group(1).encode('utf_8'), m.group(2).encode('utf_8')
