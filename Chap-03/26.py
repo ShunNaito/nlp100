643d6d35bd67b3ee0b02cb36e146782bbf344415
Shun <s.n.systems.engineer@gmail.com>
2016-07-17 19:11:39 +0900

    Add 26.py

---
+++ Chap-03/26.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+import json
+import re
+
+r = re.compile(u'\|(.*)\s=\s(.*)|(\*+\{\{lang.*)')
+
+f = open('jawiki-country.json', 'r')
+lines = f.readlines()
+f.close()
+
+for line in lines:
+    article_dict = json.loads(line)
+    if article_dict["title"] == u"イギリス":
+        England_text = article_dict["text"]
+
+lines = England_text.split("\n")
+
+basicInformationDict = {}
+
+for line in lines:
+    m = r.search(line)
+    if m is not None:
+        if m.group(3) is not None:
+            tmp = (basicInformationDict.keys()
+                   [len(basicInformationDict.keys()) - 1])
+            basicInformationDict[tmp] += m.group(3)
+        else:
+            text = m.group(2).replace("'", "")
+            basicInformationDict[m.group(1)] = text
+
+# 辞書のitems()メソッドで全てのキー(key), 値(value)をたどる
+for k, v in basicInformationDict.items():   # for/if文では文末のコロン「:」を忘れないように
+    print k.encode('utf_8'), v.encode('utf_8')
