a69ef0f187ecd7646a48c6cc022a85050d47e69f
Shun <s.n.systems.engineer@gmail.com>
2016-07-24 18:41:57 +0900

    Update 28.py

--- Chap-03/28.py
+++ Chap-03/28.py
@@ -11,8 +11,18 @@ def remove_internal_link(m):
         extracted_string = m2.group(1)
     return extracted_string.decode('utf_8')
 
+
+def remove_lang_link(m):
+    m3 = r3.search(m.group().encode('utf_8'))
+    if m3.group(3) is not None:
+        extracted_string = m3.group(3)
+    else:
+        extracted_string = m3.group(1)
+    return extracted_string.decode('utf_8')
+
 r = re.compile(u'\|(.*)\s=\s(.*)|(\*+\{\{lang.*)')
 r2 = re.compile(u'\[\[(.*?)(\|(.+?))*]\]')
+r3 = re.compile(u'\{\{(.*?)(\|(.+?))*}\}')
 
 f = open('jawiki-country.json', 'r')
 lines = f.readlines()
@@ -33,7 +43,15 @@ for line in lines:
         if m.group(3) is not None:
             officialCountry = (basicInformationDict.keys()
                                [len(basicInformationDict.keys()) - 1])
-            basicInformationDict[officialCountry] += m.group(3)
+            str_lang_link_removed = re.sub(
+                u'(\{\{(.+?)\}\})+', remove_lang_link,
+                m.group(3))
+            # print str_lang_link_removed.encode('utf_8')
+            str_HTML_tags_removed = re.sub(
+                u'(\<(.+?)\>)+', "", str_lang_link_removed)
+            str_lang_name_extracted = re.sub(
+                u'（\[\[(.+?)\]\]）', u"（" + r"\1" + u"）", str_HTML_tags_removed)
+            basicInformationDict[officialCountry] += str_lang_name_extracted
         else:
             text = m.group(2).replace("'", "")
             str_internal_link_removed = re.sub(
@@ -42,7 +60,10 @@ for line in lines:
                 u'(\<(.+?)\>)+', "", str_internal_link_removed)
             str_external_link_removed = re.sub(
                 u'(\[(.+?)\])+', "", str_HTML_tags_removed)
-            basicInformationDict[m.group(1)] = str_external_link_removed
+            str_lang_link_removed = re.sub(
+                u'(\{\{(.+?)\}\})+', remove_lang_link,
+                str_external_link_removed)
+            basicInformationDict[m.group(1)] = str_lang_link_removed
 
 # 辞書のitems()メソッドで全てのキー(key), 値(value)をたどる
 # for/if文では文末のコロン「:」を忘れないように
