42eb6ca5f59440ebab0b4830c9bc47061ae9b8cd
内藤峻 <naitoushun@common-no-iMac.local>
2016-07-28 17:48:22 +0900

    Delete getJson.py

--- Chap-03/getJson.py
+++ Chap-03/getJson.py
@@ -1,28 +1,40 @@
 # coding:utf-8
 import json
 import urllib2
+import urllib
 
 
 def EnumKeys(data):
     for key in data.keys():
-        print(key)
         if key == "imageinfo":
-        	print data[key]
-        	for i in data[key]:
-        		print i.get('url')
+            for i in data[key]:
+                print i.get('url')
         if isinstance(data[key], dict):
             EnumKeys(data[key])
         # else:
-            #値の確認
+            # 値の確認
 
-url = 'https://commons.wikimedia.org/w/api.php?format=json&action=query&titles=File:Flag%20of%20the%20United%20Kingdom.svg&prop=imageinfo&&iiprop=url'
+# url = 'https://commons.wikimedia.org/w/api.php?format=json&action=query' \
+#       '&titles=File:Flag%20of%20the%20United%20Kingdom.svg&prop=imageinfo' \
+#       '&&iiprop=url'
+urlprefix = 'https://commons.wikimedia.org/w/api.php?'
+
+query = {'format': 'json',
+         'action': 'query',
+         'titles': 'File:Flag of the United Kingdom.svg',
+         'prop': 'imageinfo'}
+
+parameter = {
+    'iiprop': 'url'
+}
+
+url = urlprefix + urllib.urlencode(query) + '&' + urllib.urlencode(parameter)
+
 try:
     r = urllib2.urlopen(url)
     root = json.loads(r.read())
     query = root["query"]
     pages = query["pages"]
-
-
     EnumKeys(root)
 
 finally:
