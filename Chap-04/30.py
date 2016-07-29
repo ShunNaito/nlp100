f67e6b3ed5dfe60f09594176fdd8972b5e5cbb32
内藤峻 <syun.naito@gmail.com>
2016-07-29 16:34:39 +0900

    Update 30.py

--- Chap-04/30.py
+++ Chap-04/30.py
@@ -2,12 +2,30 @@
 
 import MeCab
 
-f = open('neko.txt')
-lines = f.readlines()  # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
-f.close()
+fin = open('neko.txt')
+lines = fin.readlines()  # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
+fin.close()
 # lines: リスト。要素は1行の文字列データ
 
 tagger = MeCab.Tagger("-Ochasen")
+sentence_set = []
+sentence = {}
 
 for line in lines:
-    print tagger.parse(line)
+    node = tagger.parseToNode(line)
+    while node:
+        sentence["surface"] = node.surface
+        feature = node.feature.split(",")
+        sentence["base"] = feature[6]
+        sentence["pos"] = feature[0]
+        sentence["pos1"] = feature[1]
+        sentence_set.append(sentence.copy())
+        node = node.next
+
+for sentence in sentence_set:
+    print sentence["surface"]
+    print sentence["base"]
+    print sentence["pos"]
+    print sentence["pos1"]
+
+fout = open("col1.txt", "w")
