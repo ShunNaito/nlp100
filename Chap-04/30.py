a89f7e5b36e3263e953bf06b51e8ea17ebf346ef
内藤峻 <syun.naito@gmail.com>
2016-07-29 18:34:10 +0900

    update 30.py

--- Chap-04/30.py
+++ Chap-04/30.py
@@ -2,30 +2,33 @@
 
 import MeCab
 
-fin = open('neko.txt')
+fin = open('neko.txt.mecab')
 lines = fin.readlines()  # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
 fin.close()
 # lines: リスト。要素は1行の文字列データ
 
 tagger = MeCab.Tagger("-Ochasen")
-sentence_set = []
-sentence = {}
+sentence = []
+morpheme_set = []
+morpheme = {}
 
 for line in lines:
-    node = tagger.parseToNode(line)
-    while node:
-        sentence["surface"] = node.surface
-        feature = node.feature.split(",")
-        sentence["base"] = feature[6]
-        sentence["pos"] = feature[0]
-        sentence["pos1"] = feature[1]
-        sentence_set.append(sentence.copy())
-        node = node.next
+    morpheme_list = line.split('\t')
+    surface = morpheme_list[0]
+    if surface == "EOS\n":
+        morpheme_set.append(sentence)
+        sentence = []
+    else:
+        morpheme["surface"] = surface
+        feature = morpheme_list[1].split(',')
+        morpheme["base"] = feature[6]
+        morpheme["pos"] = feature[0]
+        morpheme["pos1"] = feature[1]
+        sentence.append(morpheme.copy())
 
-for sentence in sentence_set:
-    print sentence["surface"]
-    print sentence["base"]
-    print sentence["pos"]
-    print sentence["pos1"]
+for sentence in morpheme_set:
+    for morpheme in sentence:
+        print morpheme["surface"], morpheme["base"], morpheme["pos"], morpheme["pos1"]
+    print 
 
-fout = open("col1.txt", "w")
+# print morpheme_set
